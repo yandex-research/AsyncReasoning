8b2a4db6556c22f3b90e256cf9de0890202f1137
Mr-DarkTesla <yakushev.ga@phystech.edu>
2025-10-24 17:24:59 +0300

    Separated prompting and cache, state management to cache

---
+++ async_reasoning_cache.py
@@ -0,0 +1,92 @@
+from enum import Enum
+from IPython.display import display, Markdown, clear_output
+import torch
+import transformers
+import shared_cache
+from typing import Sequence
+
+
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='demo.log', encoding='utf-8', level=logging.DEBUG)
+
+# state can be "thinker_only" or "thinker_and_writer"
+class State(Enum):
+    thinker_only = 0
+    thinker_and_writer = 1
+
+class AsyncReasoningCache:
+    """Create separate blocks of LLM KV cache that are arranged depending on inference mode (thinker_only, thinker_and_writer, etc)"""
+    def __init__(self, model, tokenizer, prompting, tokenizer_kwargs=dict(), starting_state=State.thinker_only):
+        
+        self.model = model
+        self.tokenizer = tokenizer
+        self.prompting = prompting
+        self.tokenizer_kwargs = tokenizer_kwargs
+        self.device = model.device
+        self.state = starting_state
+
+        # Init all needed cache blocks
+        (self.writer_prompt, self.writer_split, self.writer_output, writer_output_for_thinker_init,
+        self.thinker_prompt, self.thinker_split, self.thinker_output, self.thinker_question, thinker_output_for_writer_init
+        ) = (shared_cache.CacheBlock(config=self.model.config) for _ in range(9))
+
+        def prefill_cache_block(text: str, blocks, write_to=None):
+            if write_to is None:
+                write_to = blocks[-1]
+            tmp_cm = shared_cache.SharedCacheManager(cache_structure=[blocks], write_to=[write_to])
+            encoded = self.tokenizer(text, **self.tokenizer_kwargs)["input_ids"].to(self.device)
+            with torch.inference_mode():
+                self.model(**tmp_cm.get_input_kwargs(encoded))
+        
+        # encode each prompt section as LLM KV cache for use in generation
+        prefill_cache_block(self.prompting.writer_prompt, [self.writer_prompt]) # <-- writes KV entries to last cache in list
+        prefill_cache_block(self.prompting.thinker_prompt, [self.thinker_prompt])
+
+        # pre-fill dummy versions of thinker / writer output prefix - only used when initializing subsequent prompts
+        prefill_cache_block(self.prompting.thinker_output_prefix, [self.writer_prompt, thinker_output_for_writer_init])
+        prefill_cache_block(self.prompting.writer_output_prefix, [self.thinker_prompt, writer_output_for_thinker_init])
+
+        prefill_cache_block(self.prompting.writer_split, [self.writer_prompt, thinker_output_for_writer_init, self.writer_split])
+        prefill_cache_block(self.prompting.thinker_split, [self.thinker_prompt, writer_output_for_thinker_init, self.thinker_split])
+        
+        prefill_cache_block(self.prompting.writer_output_prefix,
+            [self.writer_prompt, thinker_output_for_writer_init, self.writer_split, self.writer_output])
+        prefill_cache_block(self.prompting.thinker_output_prefix,
+            [self.thinker_prompt, writer_output_for_thinker_init, self.thinker_split, self.thinker_output])
+
+        # prepare cache manager for each mode: only thinker and thinker+writer in parallel - it is needed to generate in each mode
+        self.cm_thinker_only = shared_cache.SharedCacheManager(
+            cache_structure=[[self.thinker_prompt, self.writer_output, self.thinker_split, self.thinker_output]],
+            write_to=[self.thinker_output],
+        )
+        self.cm_thinker_control = shared_cache.SharedCacheManager(
+            cache_structure=[[self.thinker_prompt, self.writer_output, self.thinker_split, self.thinker_output, self.thinker_question]],
+            write_to=[self.thinker_question],
+        )
+        self.cm_thinker_and_writer = shared_cache.SharedCacheManager(
+            cache_structure=[
+                [self.writer_prompt, self.thinker_output, self.writer_split, self.writer_output],
+                [self.thinker_prompt, self.writer_output, self.thinker_split, self.thinker_output],
+            ],
+            write_to=[self.writer_output, self.thinker_output],
+        )
+    
+    # To catch and logg state change
+    def __setattr__(self, name, value):
+        if name == "state":
+            logger.debug(f'state_change to {value}')
+        super().__setattr__(name, value)
+
+    @property
+    def cache_manager(self):
+        match self.state:
+            case State.thinker_only:
+                return self.cm_thinker_only
+            case State.thinker_and_writer:
+                return self.cm_thinker_and_writer
+            case _:
+                raise ValueError(f"Unexpected state {self.state}")
+
+    def get_input_kwargs(self, **kwargs):
+        return self.cache_manager.get_input_kwargs(**kwargs)
---
+++ async_reasoning_prompting.py
@@ -0,0 +1,44 @@
+# writer (w) is worker 0, thinker (t) is worker 1
+# writer sees:  [writer_prompt, thinker_output, writer_split, writer_output]
+# thinker sees: [thinker_prompt, writer_output, thinker_split, thinker_output]
+
+class AsyncReasoningPrompting:
+    def __init__(self, problem):
+        self.writer_prompt = f"""
+<|im_start|>user
+
+You are an AI assistant that can think and write outputs concurrently.
+
+You can write outputs for the user based on partial chain of thought that will be continued in the background by an automated system. Your task is to gradually write the answer as your thoughts progress.
+
+You are given the following problem:
+{problem}
+""".strip()
+
+        self.thinker_prompt = f"""
+<|im_start|>user
+
+You are an AI assistant that can think and write outputs concurrently.
+
+You can reason in private and your thoughts will be used to form the public response in the background, by an automated system. Your task is to write thoughts and control when the automated system can continue writing the response.
+
+Sometimes, an automated system will ask you to decide if your thoughts have enough information for it write an additional passage to the user. Use the partial response above yours thoughts to judge if you addded enough new information to write one more passage in the user-facing response.
+
+- Reply "yes" if you think there is enough information to write the next passage (pararagraph, equation, etc).
+- Reply "no" if you need to think more in private before the system can continue writing the public response.
+
+Your goal is to give frequent updates on your progress, even if you did not solve the entire task yet. Reason in short paragraphs. Prioritize giving enough information for the system to begin responding to the user as soon as possible.
+
+Solve the following problem:
+{problem}<|im_end|>
+<|im_start|>assistant""".strip()
+
+        self.writer_split = " [additional thoughts will appear here]\n</think>\n"
+        self.thinker_split = " [the system will continute writing the response here]"
+
+        # writer_output and thinker_output starts with these prefixes
+        self.writer_output_prefix = f"""\nI am in Writer mode. My text is visible to the user. I focus on clear, precise expression and careful word choice. I write only what is well-reasoned and verified in my workspace. I never speculate or improvise. If my thinking shifts or reveals an error, I immediately adjust. My goal is calm, accurate, and readable output."""
+        self.thinker_output_prefix =  f"""<|im_end|>\n<|im_start|>assistant\n<think>\nI am in Thinker mode. My text is not visible to the user. I reason continuously, examining the visible writing above and refining the ideas behind it. I detect errors, test assumptions, and plan improvements. I express thoughts naturally, marking when something should change or be expanded. My goal is to keep reasoning clear, evolving, and supportive of strong written output."""
+
+        # these questions are inserted to change mode depending on model answers
+        self.thinker_control_question = "\n\nSYSTEM: Given my current progress, is there enough information to continue writing the response to the user? (yes/no):"
