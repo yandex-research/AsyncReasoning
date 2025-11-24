1bac20911c0c60d1f86272b22ebce1e509e9f757
Mr-DarkTesla <yakushev.ga@phystech.edu>
2025-11-24 21:09:59 +0300

    Removed sys.path.inserts from .py

--- evals/baseline_solver.py
+++ evals/baseline_solver.py
@@ -1,5 +1,3 @@
-import sys; sys.path.insert(0, "../.");
-
 import time
 import torch
 import transformers
--- evals/eval_math-500_split.py
+++ evals/eval_math-500_split.py
@@ -1,5 +1,3 @@
-import sys; sys.path.insert(0, "../.");
-
 import os
 from pathlib import Path
 import argparse
