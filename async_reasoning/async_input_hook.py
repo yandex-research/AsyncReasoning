c4d0492db1591a09430a2268390299d01cf27305
justheuristic <justheuristic@gmail.com>
2026-01-16 20:36:57 +0300

    device_map

--- async_reasoning/async_input_hook.py
+++ async_reasoning/async_input_hook.py
@@ -1,34 +1,26 @@
-# THIS IS TEMPORARY SOLUTION FOR ASYNC INPUTS ABLATION
-
-from async_reasoning.cache import State
 
 def async_input_hook_constructor(solver, shard_to_target, target_reminders, next_shard_every_steps, problem_shards, defer_until_boundary=False):
     def on_token(writer_tokens, thinker_tokens, token_times, eos, state):
         for shard_idx, problem_shard in enumerate(problem_shards):
             if next_shard_every_steps <= 0 or len(thinker_tokens) < next_shard_every_steps * (shard_idx + 1):
                 return
-            
-            target = "input"
-            if target in shard_to_target and solver.live_context_queue.push_counter_per_target[target] == shard_idx:
-                solver.live_context_queue.push_text(
-                    f"\n\nADDITIONAL USER INPUT: {problem_shard}\n\n",
-                    target=target,
-                    defer_until_boundary=False
-                )
 
-            target = "writer"
-            if target in shard_to_target and solver.live_context_queue.push_counter_per_target[target] == shard_idx:
-                solver.live_context_queue.push_text(
-                    f" ... [SYSTEM: additional user input detected]\n" if target in target_reminders else f"\n\nADDITIONAL USER INPUT:{problem_shard}\n\n",
-                    target=target,
-                    defer_until_boundary=defer_until_boundary
-                )
-                
-            target = "thinker"
-            if target in shard_to_target and solver.live_context_queue.push_counter_per_target[target] == shard_idx:
-                solver.live_context_queue.push_text(
-                    f" ... [SYSTEM: additional user input detected]\n"  if target in target_reminders else f"\n\nADDITIONAL USER INPUT:{problem_shard}\n\n",
-                    target=target,
-                    defer_until_boundary=defer_until_boundary
-                )
+            for target in shard_to_target:
+                if solver.live_context_queue.push_counter_per_target[target] == shard_idx:
+                    print(end=f"Sent shard {shard_idx} to {target} on step {len(thinker_tokens)}.\n", flush=True)
+                    solver.live_context_queue.push_text(
+                        f"\n\nADDITIONAL USER INPUT: {problem_shard}\n\n",
+                        target=target,
+                        defer_until_boundary=defer_until_boundary and (target != "input")
+                    )
+            for target in target_reminders:
+                assert target != "input", "can't remind to input"
+                assert target not in shard_to_target, f"Can't send reminder to {target}; already in shard_to_input"
+                if solver.live_context_queue.push_counter_per_target[target] == shard_idx:
+                    print(end=f"Sent reminder {shard_idx} to {target} on step {len(thinker_tokens)}.\n", flush=True)
+                    solver.live_context_queue.push_text(
+                        f" ... [SYSTEM: additional user input detected]\n",
+                        target=target,
+                        defer_until_boundary=defer_until_boundary
+                    )
     return on_token
